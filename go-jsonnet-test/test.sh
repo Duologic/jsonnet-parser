9a1f709e99fe955efc001a212181aed6356c6d86
Duologic <jeroen@simplistic.be>
2024-04-08 00:40:43 +0200

    test: add go-jsonnet/testdata and script to run it
    This serves as a quick test for now, the output hasn't been properly validated for each individual test yet.

---
+++ go-jsonnet-test/test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+DIRNAME="$(dirname "$0")"
+
+LEX=$(find ./vendor/github.com/google/go-jsonnet/testdata/ -name \*.jsonnet)
+PARSE=$(find ./vendor/github.com/google/go-jsonnet/testdata/ -name \*.jsonnet | \
+        grep -v error_hexnumber | \
+        grep -v import_block_literal | \
+        grep -v import_computed | \
+        grep -v importbin_block_literal | \
+        grep -v importbin_computed | \
+        grep -v importstr_block_literal | \
+        grep -v importstr_computed | \
+        grep -v insuper4 | \
+        grep -v object_comp_assert | \
+        grep -v object_comp_bad_field | \
+        grep -v object_comp_illegal | \
+        grep -v static_error_eof | \
+        grep -v syntax_error | \
+        grep -v unfinished_args
+    )
+
+for F in $LEX; do
+    echo $F
+    ../scripts/lex.sh $F > /dev/null
+done
+for F in $PARSE; do
+    echo $F
+    ../scripts/parse.sh $F > /dev/null
+done
